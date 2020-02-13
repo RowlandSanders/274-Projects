07fa45f971f5e3f10a8b53210c78beed304e00ec
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-02-12 19:06:49 -0800

    adding birthday and marathon updates

--- 274 FIles/Project1/Marathon/src/Prog1/Main.java
+++ 274 FIles/Project1/Marathon/src/Prog1/Main.java
@@ -27,7 +27,9 @@ public class Main {
         sec = input.nextInt();
         System.out.println("");
         Pace(hr,min,sec,distance,name);
+
     }
+
     public static void Pace(int hr, int min, int sec, double distance,String name){
         double eP = ((1*3600)+(59*60)+(40))/(26+(385/1760.0)); //eliud input
         double aP = ((hr*3600)+(min*60)+sec)/distance; //my input
@@ -41,6 +43,7 @@ public class Main {
         System.out.println("Your pace is "+sMin+":"+sSec);
         System.out.println("At this rate your marathon time would be "+tHr+":"+tMin+":"+tSec);
         System.out.println("Good luck with your training!");
+
         for (int i = 1;i <= 26;i++){
             int eHr = (int)((i*eP)/3600);
             int eMin = (int)(((i*eP)-(eHr*3600))/60);
@@ -49,6 +52,7 @@ public class Main {
             int aMin = (int)(((i*aP)-(aHr*3600))/60);
             int aSec = (int)((i*aP)-((aHr*3600)+(aMin*60)));
             System.out.printf("%d\t\t\t%01d:%02d:%02d\t\t\t%01d:%02d:%02d\n",i,eHr,eMin,eSec,aHr,aMin,aSec);
+            
             if (i==26){
                 int eyHr = (int)(((26+(385/1760.0))*eP)/3600);
                 int eyMin = (int)((((26+(385/1760.0))*eP)-(eyHr*3600))/60);

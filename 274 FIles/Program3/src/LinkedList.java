5e4365a5361f331ea58ce940f9cb7401139994cc
RowlandSanders <48600494+RowlandSanders@users.noreply.github.com>
2020-03-11 14:19:22 -0700

    File changes

--- 274 FIles/Program3/src/LinkedList.java
+++ 274 FIles/Program3/src/LinkedList.java
@@ -15,15 +15,20 @@ public class LinkedList {
         }
     }
 
-    public void delete(Node Nodep, LinkedList myList){     //delete node pointer
-        Random rand = new Random();
-        Node temp = Nodep.getNext();
-        Nodep.value = temp.getValue();
-        Nodep.next = temp.rand;
-        temp = null;
-
-    }
-
+    public void delete(int del) {     //delete node pointer
+        Node prev = head;
+        Node curr = head;
+        int count = 0;
+        if(del==1){
+            head = head.getNext();
+        }else{
+            while (count<del) {
+                prev = curr;
+                curr = curr.getNext();
+                count++;
+            }
+            
+        }
     }
 
     public int getSize(){
